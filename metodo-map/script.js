401d6f6038fb25f63ba64399056ca48b5485f2ec
Blazuti <gustavo.blazuti@hotmail.com>
2026-03-07 02:34:33 -0300

    estudo map

---
+++ metodo-map/script.js
@@ -0,0 +1,8 @@
+const tema = document.querySelector('.tema')
+let menup = document.querySelector(".menu")
+
+tema.addEventListener('click', () => {
+    menup.forEach(item => console.log(item))
+})
+
+
