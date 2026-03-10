fc084bf2e4e4885fc82f6adc5eee5b618240540f
Blazuti <gustavo.blazuti@hotmail.com>
2026-03-10 09:24:08 -0300

    Update script.js

--- metodo-map/script.js
+++ metodo-map/script.js
@@ -1,15 +1,7 @@
 const tema = document.querySelector('#itema')
 let menu = [...document.getElementsByClassName("menu")]
 let cont
-tema.addEventListener('click', () => menu.forEach(element => {
-    cont++
-    if (cont==1){
-    for(v of menu) {v.style.backgroundColor = 'red'}
+tema.addEventListener('click', () => { for(let t in menu){
     
-    if (cont == 2) {for(v of menu) {v.style.backgroundColor = 'blue'}
-    }}else {cont = 0}
-    
-}))
-console.log(cont)
-
-
+    console.log(t)
+} })
